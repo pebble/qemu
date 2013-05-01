4366045ee3bd504d3264f8bb004c355e78a5f4c3
Ed Maste <emaste@freebsd.org>
2013-04-30 22:27:46 -0400

    Add Micron section
    From Linux drivers/mtd/devices/m25p80.c commits:
    8da28681eb1430fb6715c7aef67001acfbbbcba5
    98a9e2450667e497246449f96eab06eb3fb4d24b

--- hw/m25p80.c
+++ hw/m25p80.c
@@ -112,6 +112,11 @@ static const FlashPartInfo known_devices[] = {
     { INFO("mx25l25635e", 0xc22019,      0,  64 << 10, 512, 0) },
     { INFO("mx25l25655e", 0xc22619,      0,  64 << 10, 512, 0) },
 
+    /* Micron */
+    { INFO("n25q128a11",  0x20bb18,      0,  64 << 10, 256, 0) },
+    { INFO("n25q128a13",  0x20ba18,      0,  64 << 10, 256, 0) },
+    { INFO("n25q256a",    0x20ba19,      0,  64 << 10, 512, ER_4K) },
+
     /* Spansion -- single (large) sector size only, at least
      * for the chips listed here (without boot sectors).
      */
