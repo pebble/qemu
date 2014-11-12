4a079a3ff4369b19e225adc301a36d89b81839a7
Ron Marianetti <ron@getpebble.com>
2014-11-12 11:33:36 -0800

    debug script

---
+++ pebble_dbg.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+EMULATOR=arm-softmmu/qemu-system-arm
+
+while getopts Sd opt; do
+    case $opt in
+    S)
+        flags="$flags -S"
+        ;;
+    d)
+        # LOG_UNIMP
+        # LOG_GUEST_ERROR
+        flags="$flags -d unimp,guest_errors"
+        ;;
+    esac
+done
+
+# lldb --
+    #-mtdblock qemu_spi_flash.bin \
+
+$EMULATOR -rtc base=localtime -machine pebble-bb2 -s $flags \
+    -pflash qemu_micro_flash.bin \
+    -serial file:uart1.log \
+    -serial file:uart2.log \
+    -serial tcp::12345,server,nowait \
+    -monitor stdio  \
+    -S  \
+    -singlestep \
+    -d out_asm,in_asm,op,op_opt,int,exec,cpu,pcall,cpu_reset,ioport,unimp,guest_errors
+    
